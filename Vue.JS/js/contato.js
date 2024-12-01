c09e0d4ee9bc4561c37747ee182dd69726c97a5f
Robertha S Rezende <eurobertharezende@gmail.com>
2024-11-30 23:58:11 -0300

    ajustes

--- Vue.JS/js/contato.js
+++ Vue.JS/js/contato.js
@@ -19,14 +19,13 @@ const PaginaContato = {
             <!-- Responsável 1 -->
             <div class="col-md-4 mb-4">
                 <div class="card">
-
                     <img src="Imagens/David.png" class="card-img-top" alt="Responsável 1">
                     <div class="card-body">
                         <h5 class="card-title">David Bryan</h5>
                         <p class="card-text">Coordenadora de Projetos</p>
+                        <p><strong>Telefone:</strong> (11) 98765-4321</p>
                         <p><strong>LinkedIn:</strong> 
-                            <a href="">David no LinkedIn</a>
-
+                            <a href="https:" target="_blank">Ana Souza no LinkedIn</a>
                         </p>
                     </div>
                 </div>
@@ -35,14 +34,13 @@ const PaginaContato = {
             <!-- Responsável 2 -->
             <div class="col-md-4 mb-4">
                 <div class="card">
-
                     <img src="Imagens/Felipe.png" class="card-img-top" alt="Responsável 2">
                     <div class="card-body">
                         <h5 class="card-title">Felipe Fernandes</h5>
                         <p class="card-text">Gerente de Marketing</p>
+                        <p><strong>Telefone:</strong> (21) 91234-5678</p>
                         <p><strong>LinkedIn:</strong> 
-                            <a href="" target="_blank">Felipe no LinkedIn</a>
-
+                            <a href="https:" target="_blank">Carlos Silva no LinkedIn</a>
                         </p>
                     </div>
                 </div>
@@ -51,14 +49,13 @@ const PaginaContato = {
             <!-- Responsável 3 -->
             <div class="col-md-4 mb-4">
                 <div class="card">
-
                     <img src="Imagens/Robertha.png" class="card-img-top" alt="Responsável 3">
                     <div class="card-body">
                         <h5 class="card-title">Robertha Rezende</h5>
-                        <p class="card-text">Estudante</p>
+                        <p class="card-text">Diretora de Comunicação</p>
+                        <p><strong>Telefone:</strong> (31) 98765-4321</p>
                         <p><strong>LinkedIn:</strong> 
-                            <a href="https:" https://www.linkedin.com/in/robertha-de-sousa-rezende-0b15b8215/">Robertha de Sousa Rezende no LinkedIn</a>
-
+                            <a href="https:" target="_blank">Fernanda Lima no LinkedIn</a>
                         </p>
                     </div>
                 </div>
@@ -69,14 +66,13 @@ const PaginaContato = {
         <div class="row">
             <div class="col-md-4 mb-4">
                 <div class="card">
-
                     <img src="Imagens/Victor.png" class="card-img-top" alt="Responsável 4">
                     <div class="card-body">
                         <h5 class="card-title">Victor Araújo</h5>
                         <p class="card-text">Supervisor de TI</p>
+                        <p><strong>Telefone:</strong> (41) 91234-6789</p>
                         <p><strong>LinkedIn:</strong> 
-                            <a href="" target="_blank">Victor no LinkedIn</a>
-
+                            <a href="https:" target="_blank">Roberto Alves no LinkedIn</a>
                         </p>
                     </div>
                 </div>
